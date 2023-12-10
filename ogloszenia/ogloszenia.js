b0323c4de4164141ce7acc1282d26992c7bf1aba
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2023-12-10 20:39:49 +0100

    Aktualizacja

--- ogloszenia/ogloszenia.js
+++ ogloszenia/ogloszenia.js
@@ -1,140 +1,12 @@
 const articles = [
     {
-        name: "Szkoła się zaczyna, pora nauki, nie imprezowania",
-        date: "piątek, 1 wrz 2023",
-        pinned: true,
-        img: "default",
-        article: [
-            `To już koniec, będzie trzeba zmykać się do swoich szkół, zostawić te pieruńsko ważne projekty, i się zajmować nauką - niestety takie chore życie uczniów. Ale spokojnie, wypuszczam małą wersję 1.1.1. Dodaje ona lepsze poruszanie suwakami przez Gamepad, i kilka ustawień. Ulepsza też wizualnie parę dodatków i trochę poprawia postć lunatone (propozycja od kolegi)`,
-            `Btw, <a href="https://discord.gg/v3w6bbtGy9" target="_blank">ten link</a> poprowadzi do serwera Discord, gdzie będzie można dać propozycje na temat postaci, czy zgłaszać błędy różnego rodzaju`,
-        ],
-    },
-    {
-        name: "<i>Ultradźwiękowo szybkie</i> poprawki z wersji 1.1",
-        date: "czwartek, 6 lip 2023",
+        name: "Nowe era ogłoszeń",
+        date: "niedziela, 10 gru 2023",
         pinned: false,
         img: "default",
         article: [
-            `Dopiero teraz zdałem sobie sprawę, że wersja 1.1 miała dosyć błędów na stabilne funkcjonowanie. Jedynie, co poprawiłem, to ustawienia oraz tłumaczenie tekstu poddawania się. Takie głupie błędy nie zauważyć...`,
-            `Ja tak jeszcze czytałem te postanowienia, które pisałem...<br />
-            Szczerze mogę powiedzieć, że nic nie działałem w tym kierunku, ale tylko dlatego, że nie mam jeszcze odpowiednich umiejętności, jak i samych możliwości. Czy uda się zrealizować je? Może tak...`,
-        ],
-    },
-    {
-        name: "Powitajcie Kiranę, Admina i Gabrysię, bo oto wersja 1.1 Starcia Internetu!",
-        date: "czwartek, 29 cze 2023",
-        pinned: false,
-        img: "https://cdn.discordapp.com/attachments/1047919900875825293/1123997006172524574/sketch-1688052140278.png",
-        article: [
-            `Jeżeli twierdziliście, że już to koniec, to się grubo mylicie.`,
-            `Przychodzę z wieścią, że Starcie Internetu w wersji 1.1 zostało wydane! Powiem wam, że się zmieniło nie do poznania. Mechanika została zmieniona, nowe postacie przybyły, i dodano jedną <i>mega</i> bardzo przydatne ustawienie.`,
-            `Trochę o postaciach...<br />
-            Dodano przed wszystkim 2 sławne postacie i postać związana z prawdziwym życiem.<br />
-            Kirana Yonome jest dość sławną VTuberką z ponad 10 tysiącami obserwującymi na <a href="https://www.twitch.tv/kirana_yonome">Twitchu</a>. Czemu jądro Internetu ją wybrało? Do tej pory nie poznamy odpowiedzi, poza tym, ze chcieli zrobić ją wojoniczką. Jest niestety jedyna w swoim wymiarze, ale nadejdzie chwila, że będzie miała towarzysza/towarzyszkę, tak jak było to z Pikachu.<br />
-            Spodziewaliście się administratora <a href="https://www.facebook.com/SuperSnacki">Super Snacki</a>? Tak, on też się pojawił. Przede wszystkim reklamuje kabanosy Sokołów memami i jest praktycznie w tym najlepszy! Jest idolem Gabrysi Sotoły, co dopiero zaczęła swoją przygodę w tym fanpage'u.<br />
-            Poza nimi są jeszcze 4 postacie (glalirthor, lunatone, theDarkLord i twinz), ale to już polecam wam samemu odkryć, co każdy robi.`,
-            `Te przydatne ustawienie, o którym mówiłem, jest zmiana języka! Dzięki temu możliwe, że się jakoś rozrośnie. Oczywiście tłumaczenie nie działa i nie jest zweryfikowane w 100%, ale jeszcze powinno się to zmienić <i>jakoś<i>.`,
-            `Jeżeli ktoś chce zobaczyć listę postaci, to <a href="charaList.html">ta strona</a> na to pozwala.`,
-        ],
-    },
-    {
-        name: "Starcie Internetu 1.0",
-        date: "wtorek, 11 kwi 2023",
-        pinned: false,
-        img: "https://cdn.discordapp.com/attachments/1094716831392092261/1095312905907486782/sketch-1681213312199.png",
-        article: [
-            `<i>Kiedyś, kiedy istniał Internet, istniała grupka przyjaciół, którzy zostali odrzuceni czy zapomnieni. Przede wszystkim potrafili walczyć.<br />
-            Pewnego dnia rdzeń zaczął się dziwnie zachowywać. Przede wszystkim utworzyło dziwną strefę waleczną i zaczęło tworzyć tzw. "czerwony kod". Powodował, że inne postacie z kultowych gier zostali niespodziewanie porywani.<br />
-            Na przykład trójka osób z drużyny z <u>Rocket League</u> grała dość ważny mecz. Drużyna składała się wtedy z Octana, Dominusa oraz Hotshota. Każdy walczył jak najlepiej. Po 4 minutach piłka zamieniła się w wyrwę próbująca porywać każdą możliwą postać. Wszyscy z obecnych na arenie próbowali przetrwać tą apokalipsę - niestety wspomniana drużyna mimo starań została porwana przez tą wyrwę. Tak zakończono mecz.<br />
-            Podobnie wyrwa porwała Pikachu oraz kilku stickmanów (tych słynnych i nieznanych) i dalej próbuje znaleźć kolejne ofiary, aby stworzyć tzw. ultra bijatykę.<br />
-            I tak powstało Starcie Internetu...</i>`,
-            `Witajcie gracze w wersji 1.0 Starcia Internetu! Ta gra polega na walce między postaciami. Prosta gra z animacjami i dźwiękami.`,
-            `W tej grze możesz znaleźć ciekawe postacie zapożyczone z gier/animacji.<br />
-            - Przykładem może być theChosenOne i theSecondComing Są to postacie z serii animacji AvA/AvM. Te postacie są obdarzone potężnymi umiejętnościami.<br />
-            -- theChosenOne pochodzi z "Animator vs. Animation III", która została stworzona przez animatora. Charakteryzuje się ona niezwykłymi umiejętnościami, które pozwoliły jej stawić czoła animatorowi i jego animacji. Posiada niezwykłą siłę, umiejętności walki i manipulacji programami komputerowymi, co czyni ją bardzo groźnym przeciwnikiem. Pomimo swojej potęgi, theChosenOne wykazuje także empatię i troskę o innych, co czyni ją godną podziwu postacią.<br />
-            -- theSecondComing to postać z kontynuacji animacji "Animator vs. Animation IV", która pojawiła się po tym, jak animator pokonał oryginalnego theChosenOne. Charakteryzuje się ona podobnymi umiejętnościami co jej poprzednik, ale jest jeszcze bardziej potężna i trudniejsza do pokonania. Posiada umiejętności takie jak tworzenie wielu kopii samej siebie, kontrolowanie innych animacji i manipulacja złożonymi programami komputerowymi. Pomimo swojej potęgi, theSecondComing wykazuje także inteligencję i strategiczne myślenie, co czyni ją jeszcze bardziej niebezpieczną dla animatora.`,
-            `Od tej wersji możesz zobaczyć gwiezdne moce. Dzięki temu każda postać posiada własną specjalną umiejętność.<br />
-            Jeżeli Ci myszką trudniej, obczaj granie Gamepadem! Dzięki temu możliwe jest zwiększenie grupy graczy.<br />
-            Przydatne informacje:<br />
-            - jeżeli widzisz pasek przewijania, ruszenie prawego joysticka w górę/dół spowoduje poruszenie paska i przewijanie<br />
-            - jeżeli masz błąd ruszania joysticka, spróbuj RT + LT + Select - wykona to akcję "miękkiego resetu". Aby stworzyć "twardy reset", użyj kombinacji RT + LT + B/koło + Select<br />`,
-            `Sam się przekonaj o nowościach! Życzę wszystkim miłego dnia/miłej nocy!`,
-        ],
-    },
-    {
-        name: "Aktualizacja 0.3.2 + informacja, jak naprawić błąd logowania (telefon)",
-        date: "środa, 25 sty 2023",
-        pinned: false,
-        img: "default",
-        article: [
-            `Dzień dobry. Ktoś tu jeszcze żyje?<br />
-            Jak zawsze przychodzę z kolejną aktualizacją i poradnikiem, jak naprawić problem z logowaniem na telefonach`,
-            `<u>Aktualizacja</u><br />
-            Kolejna zmiana działania pod kątem zbierania punktów (przez ataki), poprawki i zmiany postaci:<br />
-            > theSecondComing zostaje ulepszony pod kątem ataków,<br />
-            > theChosenOne zostaje obniżony pod kątem początkowego hp i ataku,<br />
-            > diamentowyDominus zostaje ulepszony pod kątem ataku i jego mnożnika.<br />
-            > pikachu zostaje obniżony pod kątem ataku i początkowego hp.<br />
-            > sylwestrowyOctane zostaje podwyższony pod kątem ataku.<br />
-            > kira zostaje obniżona pod kątem początkowego hp.<br />`,
-            `<u>Poradnik</u><br />
-            Jeżeli zamierzasz grać na telefonie, przeszkodą może się stać logowanie - ona tworzy tzw. efekt "tunelu", polegającym na ciągłym logowaniu się. Po to jest ten <i>nawet przypięty</i> artykuł, aby grać bez przeszkód.<br />
-            Pierwsze do zrobienia jest włączenie zewnętrznej przeglądarki (za wewnętrzną może odrzucić próbę logowania).<br />
-            Kolejnym jest włączenie trybu komputerowego i się zalogowanie przez Google.<br />
-            Potem, po zalogowaniu, możecie się przełączyć na mobilną wersję i grać!`,
-            `Miłego dnia!`,
-        ],
-    },
-    {
-        name: "Aktualizacja 0.3.1, czyli pogaduszki o postaciach",
-        date: "piątek, 20 sty 2023",
-        pinned: false,
-        img: "default",
-        article: [
-            `Witajcie, gracze! Dziś ta aktualizacja powoduje zmiany w postaciach, dodaje kolejne postacie oraz zmienia same granie.`,
-            `Na mocy przybrali:<BR />
-            > habby<br />
-            -> HP podstawowe zwiększyło się o 10<br />
-            -> Zwiększone ataki<br />
-            > trajom<br />
-            -> HP podstawowe zwiększyło się o 600<br />
-            -> Zwiększone ataki<br />
-            > theSecondComing<br />
-            -> HP podstawowe zwiększyło się o 300<br />
-            -> DEF na odporność mocy został zwiększony o 1000<br />
-            > Sylwestrowy Octane<br />
-            -> Zmniejszone BTP`,
-            `Pojawili się też postacie, które straciły energię kodu:<BR />
-            > theChosenOne<br />
-            -> HP podstawowe zmniejszyło się o 500<br />`,
-            `Teraz kolej na nowych postaci:
-            > pikachu [ Pokémon (NOWY WYMIAR) ] <br />
-            > kira [ Starcie internetu ]<br />
-            > paty [ Stick'y-land ]<br />
-            > havier [ Stick'y-land ]<br />`,
-            `Prócz tego mechanika się zmieniła. Zamiast gradientu pojawiło się zmieniające się tło. Do tego się zmienił system leczenia.<br />
-            + można pokazać nowe audiosy:<br />
-            Atak<br /><audio controls src="https://patyczakus.github.io/starcie-internetu/audios/attack.wav"></audio><br />
-            Ktytyczny cios<br /><audio controls src="https://patyczakus.github.io/starcie-internetu/audios/critical.wav"></audio>`,
-        ],
-    },
-    {
-        name: "Pierwszy artykuł do wersji 0.3",
-        date: "czwartek, 19 sty 2023",
-        pinned: false,
-        img: "default",
-        article: [
-            `Witajcie, mili gracze!<BR />
-            Przyszedłem z Wami z dość dużą aktualizacją, m. in. dodałem artykuły/ogłoszenia.<BR />
-            Poza tym są jeszcze inne nowości:<BR />
-            > Naprawa błędów (oraz próba naprawy błędu spowodowanego efektem tunelu - chodzi o błąd, w którym odrzucało logowanie)<BR />
-            > Animacja otwierania skrzyń<BR />
-            > Zmiana kolejności postaci`,
-            `Też są postanowienia:<BR />
-            > Jak zwykle dużo postaci<BR />
-            > Klany, gildie, kluby, gangi - jak zwał, tak zwał<BR />
-            > Misje<BR />
-            > Sklep`,
+            `Te głoszenia tracą swoją moc na rzecz serwera support. Tam też można skontaktować się z działem wsparcia.<br />Tym razem usunąłem wszystkie dane kont, a nie je podmieniłem, także każdy ma podmienione dane na start!`,
+            `<iframe src="https://discord.com/widget?id=1173722642004574359&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>`,
         ],
     },
 ]
@@ -145,9 +17,7 @@ function create() {
         if (!articles[i].pinned)
             document.body.innerHTML += `<div id="article">
         <div class="header"><img src="${
-            articles[i].img == "default"
-                ? "https://cdn.discordapp.com/attachments/1047919900875825293/1064986147643740223/sketch-1673982624892.png"
-                : articles[i].img
+            articles[i].img == "default" ? "https://cdn.discordapp.com/attachments/1047919900875825293/1064986147643740223/sketch-1673982624892.png" : articles[i].img
         }" /><span>${articles[i].name}<div style="font-size: 50%">${articles[i].date}</div></span></div>
         <div><p>${articles[i].article.join("</p><p>")}</p></div>
         </div>
@@ -156,9 +26,7 @@ function create() {
             document.body.innerHTML =
                 `<div id="article">
         <div class="header"><img src="${
-            articles[i].img == "default"
-                ? "https://cdn.discordapp.com/attachments/1047919900875825293/1064986147643740223/sketch-1673982624892.png"
-                : articles[i].img
+            articles[i].img == "default" ? "https://cdn.discordapp.com/attachments/1047919900875825293/1064986147643740223/sketch-1673982624892.png" : articles[i].img
         }" /><span>📌 ${articles[i].name}<div style="font-size: 50%">${articles[i].date}</div></span></div>
         <div><p>${articles[i].article.join("</p><p>")}</p></div>
         </div>
@@ -169,9 +37,7 @@ function create() {
         articles[articles.length - 1].img == "default"
             ? "https://cdn.discordapp.com/attachments/1047919900875825293/1064986147643740223/sketch-1673982624892.png"
             : articles[articles.length - 1].img
-    }" /><span>${articles[articles.length - 1].name}<div style="font-size: 50%">${
-        articles[articles.length - 1].date
-    }</div></span></div>
+    }" /><span>${articles[articles.length - 1].name}<div style="font-size: 50%">${articles[articles.length - 1].date}</div></span></div>
     <div><p>${articles[articles.length - 1].article.join("</p><p>")}</p></div>
     </div>`
 }
